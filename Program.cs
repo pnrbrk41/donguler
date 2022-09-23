d560467c933d0659560fb918b1d044073f2dc3bb
pnrbrk41 <113897255+pnrbrk41@users.noreply.github.com>
2022-09-23 23:00:50 +0300

    duzenlendi
    duzenlendi

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+
+    
+
+	namespace switchh
+	{
+
+
+   class Program
+   
+   {
+      static void Main (string[] args )
+      {
+        Console.WriteLine("Lütfen bir sayı giriniz:");
+        int sayac = int.Parse(Console.ReadLine());
+        for( int i=1; i<= sayac; i++)
+        {
+            if(i%2 == 1)
+            Console.WriteLine(i);
+
+        }
+      int tektoplam= 0;
+      int cifttoplam= 0;
+      for( int i=1; i<=1000; i++)
+{
+    if( i%2 == 1)
+    tektoplam += 1;
+
+    else
+    cifttoplam += 1;
+
+}
+      Console.WriteLine("Tek toplam: " +tektoplam);
+      Console.WriteLine("Çift Toplam:" +cifttoplam);
+
+
+      }
+   }
+    }
